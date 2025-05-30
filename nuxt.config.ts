01a15afd18d6473a685e3940c0fffeea1dbcd739
Glebanka <glebankaplay905@gmail.com>
2025-05-30 11:18:26 +0600

    Add verbose logging and enable source maps in Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  
+  logLevel: 'verbose', // Options: 'silent', 'info', 'verbose'
+  sourcemap: {
+    server: true,
+    client: true
+  },
 
   ssr: true,
   debug: true,
