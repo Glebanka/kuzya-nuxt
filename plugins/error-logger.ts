32cf1833c37f25b6c8244948736e3d0caad7ccff
Glebanka <glebankaplay905@gmail.com>
2025-05-30 11:39:20 +0600

    Add error logging plugin to handle Vue component errors

---
+++ plugins/error-logger.ts
@@ -0,0 +1,9 @@
+import { defineNuxtPlugin } from '#app'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.config.errorHandler = (err, vm, info) => {
+    // vm — это Vue ComponentInstance или undefined
+    const name = (vm as any)?.$options?.name || (vm as any)?.type?.name || 'Unknown'
+    console.error(`❌ [Vue Error] Component: ${name}\nInfo: ${info}`, err)
+  }
+})
